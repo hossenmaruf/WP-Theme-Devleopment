55d725e5a83df32e4b7627f0f7b2b632f7b47173
hossenmaruf <severusmaruf740@gmail.com>
2022-08-28 05:55:40 +0600

    blog_post

--- include/default.php
+++ include/default.php
@@ -8,3 +8,25 @@ add_theme_support('title-tag');
 
 add_theme_support('post-thumbnails', array('page', 'post'));
 add_image_size('post-thumbnails', 950, 350, true);
+
+
+function m_excerpt_more($more)
+{
+
+    // get_permalink(  $post->ID)
+    return '<br> <br> <a class="redmore" href="'.get_permalink() . '">' . 'Read More' . '</a>';
+}
+
+
+
+
+add_filter('excerpt_more', 'm_excerpt_more');
+
+
+function m_excerpt_lenght($leggth)
+{
+
+    return 40;
+}
+
+add_filter('excerpt_length', 'm_excerpt_lenght', 999);
