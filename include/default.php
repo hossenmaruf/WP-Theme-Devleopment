b930052fb77d3ca45ef0683b40f0bc2c957cf66a
hossenmaruf <severusmaruf740@gmail.com>
2022-08-29 02:31:24 +0600

    page_nav

--- include/default.php
+++ include/default.php
@@ -14,7 +14,7 @@ function m_excerpt_more($more)
 {
 
     // get_permalink(  $post->ID)
-    return '<br> <br> <a class="redmore" href="'.get_permalink() . '">' . 'Read More' . '</a>';
+    return '<br> <br> <a class="redmore" href="' . get_permalink() . '">' . 'Read More' . '</a>';
 }
 
 
@@ -30,3 +30,26 @@ function m_excerpt_lenght($leggth)
 }
 
 add_filter('excerpt_length', 'm_excerpt_lenght', 999);
+
+
+//page nev function
+
+
+function m_pagenav()
+{
+    global $wp_query, $wp_rewrite;
+    $pages = '';
+    $max = $wp_query->max_num_pages;
+    if (!$current = get_query_var('paged')) $current = 1;
+    $args['base'] = str_replace(999999999, '%#%', get_pagenum_link(999999999));
+    $args['total'] = $max;
+    $args['current'] = $current;
+    $total = 1;
+    $args['prev_text'] = 'Prev';
+    $args['next_text'] = 'Next';
+    if ($max > 1) echo '</pre>
+      <div class="wp_pagenav">';
+    if ($total == 1 && $max > 1) $pages = '<p class="pages"> Page ' . $current . '<span>of</span>' . $max . '</p>';
+    echo $pages . paginate_links($args);
+    if ($max > 1) echo '</div><pre>';
+}
--- index.php
+++ index.php
@@ -1,75 +1,46 @@
 <?php
 /*
-* This template for displaying the header
+* The main template file
 */
-
-
-get_header();
-
-?>
-
+get_header(); ?>
 
 <section id="body_area">
-
   <div class="container">
-
     <div class="row">
-
       <div class="col-md-9">
-
-        <?php
-
-        if (have_posts()) :
-          while (have_posts()) : the_post();
-        ?>
-
+        <?php if (have_posts()) :
+          while (have_posts()) : the_post(); ?>
 
             <div class="blog_area">
-
-              <?php the_excerpt();     ?>
-
               <div class="post_thumb">
-
-                <h2> <a href=" <?php the_permalink(); ?>"> <?php echo the_post_thumbnail('post-thumbnails'); ?> </a> </h2>
-
+                <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
               </div>
-
               <div class="post_details">
-
-                <h2> <a href=" <?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
-
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
               </div>
-
-
             </div>
-
-        <?php
-
-          endwhile;
+        <?php endwhile;
         else :
-          _e('no post found');
-        endif;
+          _e('No post found');
+        endif; ?>
 
 
 
-        ?>
-
+        <div id="page_nav">
+          <?php if ('m_pagenav') {
+            m_pagenav();
+          } else { ?>
+            <?php next_posts_link(); ?>
+            <?php previous_posts_link(); ?>
+          <?php } ?>
+        </div>
       </div>
-
-
       <div class="col-md-3">
-
-        <h1> this is side bar</h1>
-
+        <h2>This is sidebar area</h2>
       </div>
-
-
-
     </div>
   </div>
-
-
 </section>
 
-
-<?php get_footer();    ?>
+<?php get_footer(); ?>
