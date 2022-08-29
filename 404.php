1a453addd6c37dd629dc5837204de0eb18d49eae
hossenmaruf <severusmaruf740@gmail.com>
2022-08-30 03:41:42 +0600

    404

--- 404.php
+++ 404.php
@@ -1,30 +1,29 @@
 <?php
 /*
-* This template for displaying the header
+* The template for 404
 */
-
-
-get_header();
-
-?>
-
+get_header(); ?>
 
 <section id="body_area">
-
   <div class="container">
-
     <div class="row">
+      <div class="col-md-12 error_page   ">
 
-      <div class="col-md-12">
+        <p> 404 error - page not found </p>
 
-        <?php the_content();     ?>
+        <div class="error_search">
+
+          <?php get_search_form(); ?>
+
+        </div>
+
+        <a href="<?php echo home_url();?> " class = "homepage"> Homepage </a>
 
       </div>
     </div>
+
   </div>
-
-
+  </div>
 </section>
 
-
-<?php get_footer();    ?>
+<?php get_footer(); ?>
